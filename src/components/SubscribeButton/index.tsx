507553a2785e02dcc565c146dfb5345e002ca1ae
Wallace Saturnino <wallacesat@live.co.uk>
2022-02-08 21:47:56 -0300

    Implementa botão de subscribe

---
+++ src/components/SubscribeButton/index.tsx
@@ -0,0 +1,10 @@
+import styles from './styles.module.scss'
+
+export default function SubscribeButton() {
+
+  return (
+    <button type="button" className={styles.subscribeButton}>
+      Subscribe now
+    </button>
+  )
+}
